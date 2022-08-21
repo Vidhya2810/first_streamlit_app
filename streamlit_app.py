e0198ba079b5e85eb88ad491f3cb91cc1f089b6d
Vidhya2810 <87438949+Vidhya2810@users.noreply.github.com>
2022-08-21 16:12:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,5 +64,5 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_response)
 
